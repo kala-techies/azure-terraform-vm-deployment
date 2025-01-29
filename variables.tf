3292c8e96453a2c085b8a88307c0bb277a215a96
kalandar <shaik.kalandar20@gmail.com>
2025-01-30 03:20:23 +0530

    un-necessary files code removed

--- variables.tf
+++ variables.tf
@@ -1,48 +1,26 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
 variable "admin_password" {
   description = "Admin password for the VM"
   type        = string
- sensitive   = true
+  sensitive   = true
 }
 
 variable "location" {
   description = "Azure Region"
   type        = string
   default     = "East US"
-  
 }
 
 variable "vm_size" {
   description = "Size of the VM"
   type        = string
-  default     = "Standard_DS1_v2" 
+  default     = "Standard_DS1_v2"
 }
 
 variable "public_ip_allocation_method" {
-    description = "Allocation method for the Public IP"
-    type        = string
-    default     = "Static"
-  }
+  description = "Allocation method for the Public IP"
+  type        = string
+  default     = "Static"
+}
 
 variable "admin_username" {
   description = "Admin username for the VM"
