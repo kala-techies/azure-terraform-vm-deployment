b8b0f9b9b5b8d062534f7fa42db409cff5cf6234
kalandar <shaik.kalandar20@gmail.com>
2025-01-29 17:58:58 +0530

    providers file updated with 3.0 version

--- providers.tf
+++ providers.tf
@@ -1,17 +1,12 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+    required_providers {
+        azurerm = {
+        source  = "hashicorp/azurerm"
+        version = ">= 3.0"
+        }
     }
-  }
+}
 
-#   backend "azurerm" {
-#     resource_group_name  = "terraform-state-rg"  # Replace with your resource group name
-#     storage_account_name = "tfstate12345"        # Replace with your storage account name
-#     container_name       = "tfstate"             # Replace with your container name
-#     key                  = "terraform.tfstate"   # Name of the state file
-  }
 provider "azurerm" {
-  features {}
+    features {}
 }
